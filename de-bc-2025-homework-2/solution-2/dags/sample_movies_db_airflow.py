ac246809de5e1ae52a2efaeeb5b805b08bab8781
Mikail Saltan <mikailsaltan@hotmail.com>
2025-02-09 18:44:41 +0300

    fix network

--- de-bc-2025-homework-2/solution-2/dags/sample_movies_db_airflow.py
+++ de-bc-2025-homework-2/solution-2/dags/sample_movies_db_airflow.py
@@ -8,16 +8,16 @@ import pandas as pd
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2023, 1, 1),
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'start_date': datetime(2025, 2, 9),
+    #'retries': 1,
+    #'retry_delay': timedelta(minutes=5),
 }
 
 dag = DAG(
-    'transfer_db_data',
+    'transfer_db_data_',
     default_args=default_args,
     description='Transfer data from source to destination',
-    schedule_interval='@once',
+    schedule_interval='@hourly',
 )
 
 def transfer_table_data(table_name):
@@ -25,22 +25,28 @@ def transfer_table_data(table_name):
     postgres_source_hook = PostgresHook(postgres_conn_id='postgres_source')
     postgres_destination_hook = PostgresHook(postgres_conn_id='postgres_destination')
     df = postgres_source_hook.get_pandas_df(f"SELECT * FROM {table_name};")  
+    conn = postgres_destination_hook.get_conn()
+    cursor = conn.cursor()
+    cursor.execute(f" truncate table {table_name}" )
     # Insert data into MySQL
     postgres_destination_hook.insert_rows(table=table_name, rows=df.values.tolist(), target_fields=df.columns.tolist())
 
 # Define tasks for each table
-tables = ['users', 'films', 'film_category', 'actors', 'film_actors']
+tables = ['users', 'films', 'film_category', 'actors', 'film_actors'] 
+tasks = {}
 
+# Loop through the tables and create tasks
 for table in tables:
-    task = PythonOperator(
-        task_id=f'transfer_{table}',
+    task_id = f'transfer_data_{table}'
+    tasks[table] = PythonOperator(
+        task_id=task_id,
         python_callable=transfer_table_data,
         op_kwargs={'table_name': table},
-        dag=dag,
+        dag=dag
     )
 
-    # Set task dependencies if needed
-    if table == 'users':
-        task
-    else:
-        task.set_upstream(f'transfer_{tables[tables.index(table)-1]}')
+# Set up dependencies (optional, if you want sequential execution)
+for i in range(1, len(tables)):
+    tasks[tables[i]].set_upstream(tasks[tables[i - 1]])
+
+ 
